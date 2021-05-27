ec3b4334ddc8ca6ea40d30ea92b192a7a3dbb35c
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-27 13:01:26 -0500

    more pagination functionality

--- client/src/components/tables/TablePagination.js
+++ client/src/components/tables/TablePagination.js
@@ -1,21 +1,40 @@
 import './TablePagination.css';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const TablePagination = ({ entryStart, entryEnd, totalResults, totalPages, currentPage, getParksData }) => {
   return (
     <div>
       <div>
         Showing {entryStart} to {entryEnd} of {totalResults} results
+        <IconButton
+          onClick={() => getParksData(currentPage - 1, 10)}
+          aria-label="previous-page"
+          disabled={currentPage === 1 ? true : false}
+        >
+          <ChevronLeftIcon />
+        </IconButton>
+        <IconButton
+          onClick={() => getParksData(currentPage + 1, 10)}
+          aria-label="next-page"
+          disabled={currentPage === totalPages ? true : false}
+        >
+          <ChevronRightIcon />
+        </IconButton>
       </div>
       <div>
-        {Array.from(Array(totalPages)).map((x, index) => (
-          <div
-            onClick={() => getParksData(index + 1, 10)}
-            className={`page-btn ${index + 1 === currentPage && 'active-page'}`}
-            key={index}
-          >
-            {index + 1}
-          </div>
-        ))}
+        {Array.from(Array(totalPages)).map((x, index) =>
+          index + 1 === currentPage ? (
+            <div className="page-btn active-page" key={index}>
+              {index + 1}
+            </div>
+          ) : (
+            <div onClick={() => getParksData(index + 1, 10)} className="page-btn not-active" key={index}>
+              {index + 1}
+            </div>
+          ),
+        )}
       </div>
     </div>
   );
