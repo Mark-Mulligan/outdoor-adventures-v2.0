5a9051120508a894df337ca5c56f406fdf618768
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-26 19:45:15 -0500

    break paganation into function

--- controllers/parks.js
+++ controllers/parks.js
@@ -1,5 +1,27 @@
 const connection = require('../config/connection');
 
+const formateQueryData = (data, totalResults, limit, endIndex, page) => {
+  const formattedData = {};
+  formattedData.totalPages = Math.ceil(totalResults / limit);
+
+  if (endIndex < totalResults) {
+    formattedData.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (page - 1 > 0) {
+    formattedData.previous = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  formattedData.results = data;
+  return formattedData;
+};
+
 exports.getParks = async (req, res) => {
   const limit = parseInt(req.query.limit, 10);
   const page = parseInt(req.query.page, 10);
@@ -7,30 +29,11 @@ exports.getParks = async (req, res) => {
   const offset = (page - 1) * limit;
   const endIndex = page * limit;
 
-  const queryString = `SELECT *, count(*) OVER() AS results from parks LIMIT ${limit} OFFSET ${offset};`;
+  const queryString = `SELECT *, count(*) OVER() AS totalResults from parks LIMIT ${limit} OFFSET ${offset};`;
 
   try {
     const [result] = await connection.promise().query(queryString);
-
-    const data = {};
-    console.log(result);
-    data.totalPages = Math.ceil(result[0].results / limit);
-
-    if (endIndex < result[0].results) {
-      data.next = {
-        page: page + 1,
-        limit,
-      };
-    }
-
-    if (page - 1 > 0) {
-      data.previous = {
-        page: page - 1,
-        limit,
-      };
-    }
-
-    data.results = result;
+    const data = formateQueryData(result, result[0].totalResults, limit, endIndex, page);
     res.status(200).json(data);
   } catch (e) {
     console.log(e);
