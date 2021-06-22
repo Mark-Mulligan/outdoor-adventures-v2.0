71343ae4f2ae7e0be7e29da4331e22652ccfcb52
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-06-21 23:45:40 -0500

    more on contacts section

--- client/src/components/parkInfo/Contact/index.js
+++ client/src/components/parkInfo/Contact/index.js
@@ -1,20 +1,20 @@
 const Contact = ({ contactInfo }) => {
   return (
-    <div>
+    <div className="container">
       <h3>Contact Info</h3>
-      <div>
-        <p>Email</p>
-        {contactInfo.emailAddresses.map((email) => (
-          <p key={email.emailAddress}>{email.emailAddress}</p>
-        ))}
-      </div>
-      <div>
-        <p>Phone</p>
-        {contactInfo.phoneNumbers
-          .filter((number) => number.type === 'Voice')
-          .map((result) => {
-            return <p key={result.phoneNumber}>{result.phoneNumber}</p>;
-          })}
+      <div className="row">
+        <div className="col">
+          {contactInfo.phoneNumbers
+            .filter((number) => number.type === 'Voice')
+            .map((result) => {
+              return <p key={result.phoneNumber}>Phone: {result.phoneNumber}</p>;
+            })}
+        </div>
+        <div className="col">
+          {contactInfo.emailAddresses.map((email) => (
+            <p key={email.emailAddress}> Email: {email.emailAddress}</p>
+          ))}
+        </div>
       </div>
     </div>
   );
