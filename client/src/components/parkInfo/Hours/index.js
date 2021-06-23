8ca00b77aea0bc83320c350a687b6f9097dcba27
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-06-22 20:35:26 -0500

    added more formatting to hours

--- client/src/components/parkInfo/Hours/index.js
+++ client/src/components/parkInfo/Hours/index.js
@@ -1,36 +1,57 @@
-const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+import './Hours.css';
+
+const days = [
+  { accessor: 'monday', value: 'Mon' },
+  { accessor: 'tuesday', value: 'Tue' },
+  { accessor: 'wednesday', value: 'Wed' },
+  { accessor: 'thursday', value: 'Thu' },
+  { accessor: 'friday', value: 'Fri' },
+  { accessor: 'saturday', value: 'Sat' },
+  { accessor: 'sunday', value: 'Sun' },
+];
 
 const Hours = ({ operatingHours }) => {
   return (
     <div className="container">
       <h3>Operating Hours</h3>
 
-      <div className="row">
-        <div className="col">
-          <p>Standard Hours</p>
-          <ol className="no-space-list">
-            {days.map((day) => {
-              return (
-                <li key={day}>
-                  {day} {operatingHours[0].standardHours[day]}
-                </li>
-              );
-            })}
-          </ol>
-        </div>
-
-        <div className="col">
-          <p>Holiday Hours</p>
-          <ul className="no-space-list">
-            {operatingHours[0].exceptions.map((exception) => {
-              return (
-                <li key={exception.startDate}>
-                  {exception.name} ({exception.startDate}) {exception.exceptionHours.monday}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+      <div>
+        {operatingHours.map((setOfHours) => {
+          return (
+            <div key={setOfHours.description} className="row">
+              <p>{setOfHours.description}</p>
+              <div className="col">
+                <p>Standard Hours</p>
+                <ol className="no-space-list">
+                  {days.map((day) => {
+                    return (
+                      <li key={day.value}>
+                        <span className="day-span">{day.value}</span>
+                        <span>{operatingHours[0].standardHours[day.accessor]}</span>
+                      </li>
+                    );
+                  })}
+                </ol>
+              </div>
+              <div className="col">
+                <p>Holiday Hours</p>
+                <ul className="no-space-list">
+                  {setOfHours.exceptions.map((exception) => {
+                    return (
+                      <li key={exception.startDate}>
+                        <div>{exception.name}</div>
+                        <div>
+                          {exception.startDate} - {exception.endDate}
+                        </div>
+                        <div>{exception.exceptionHours.monday}</div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
