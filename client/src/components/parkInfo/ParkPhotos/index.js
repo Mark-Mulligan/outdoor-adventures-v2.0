3db44cad30965557c0670b7b6f7f6e699422d856
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-06-23 09:47:23 -0500

    more work on photos section

--- client/src/components/parkInfo/ParkPhotos/index.js
+++ client/src/components/parkInfo/ParkPhotos/index.js
@@ -1,17 +1,23 @@
+import Gallery from 'react-grid-gallery';
+
 const ParkPhotos = ({ photos }) => {
+  const formatPhotos = () => {
+    return photos.map((photo) => {
+      return {
+        src: photo.url,
+        thumbnail: photo.url,
+        thumbnailWidth: 320,
+        thumbnailHeight: 174,
+      };
+    });
+  };
+
+  console.log(photos);
+
   return (
     <div id="photos" className="container">
       <h2>Photos</h2>
-      <hr />
-      <div className="row">
-        {photos.map((photo) => {
-          return (
-            <div key={photo.title} className="col-xl-4 col-lg-6 col-12 mb-4">
-              <img className="img-fluid" src={photo.url} alt={photo.altText} />
-            </div>
-          );
-        })}
-      </div>
+      <Gallery images={formatPhotos()} />
     </div>
   );
 };
