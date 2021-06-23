934a13ce8af8d012a7e614c4c974bbc7f6cef8c7
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-06-22 23:58:38 -0500

    more work on photos section

--- client/src/components/parkInfo/ParkPhotos/index.js
+++ client/src/components/parkInfo/ParkPhotos/index.js
@@ -1,12 +1,12 @@
 const ParkPhotos = ({ photos }) => {
   return (
-    <div id="#photos" className="container">
+    <div id="photos" className="container">
       <h2>Photos</h2>
       <hr />
       <div className="row">
         {photos.map((photo) => {
           return (
-            <div key={photo.title} className="col-md-4 col-sm-6 col-12">
+            <div key={photo.title} className="col-xl-4 col-lg-6 col-12 mb-4">
               <img className="img-fluid" src={photo.url} alt={photo.altText} />
             </div>
           );
