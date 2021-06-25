1f4bbe3c5fa519e35ac874bb4b358beb2fe3828a
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-06-24 20:01:55 -0500

    image modal

--- client/src/components/parkInfo/ParkPhotos/index.js
+++ client/src/components/parkInfo/ParkPhotos/index.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import Gallery from 'react-grid-gallery';
 import './ParkPhotos.css';
 
 const ParkPhotos = ({ photos }) => {
+  const [photoIndex, setPhotoIndex] = useState(-1);
+
   const formatPhotos = () => {
     return photos.map((photo) => {
       return {
@@ -11,16 +14,29 @@ const ParkPhotos = ({ photos }) => {
     });
   };
 
+  const handlePhotoClick = (event) => {
+    setPhotoIndex(parseInt(event.target.getAttribute('data-photoindex')));
+  };
+
   console.log(photos);
 
   return (
     <div id="photos" className="container">
       <h2>Photos</h2>
-      <div>
-        {photos.map((photo) => (
-          <img className="grid-image" key={photo.url} src={photo.url} alt={photo.altText} />
+      <div className="grid-images">
+        {photos.map((photo, index) => (
+          <img
+            onClick={handlePhotoClick}
+            className="grid-image"
+            data-photoindex={index}
+            key={photo.url}
+            src={photo.url}
+            alt={photo.altText}
+          />
         ))}
       </div>
+
+      <div>{photoIndex >= 0 && <img src={photos[photoIndex].url} alt={photos[photoIndex.altText]} />}</div>
     </div>
   );
 };
