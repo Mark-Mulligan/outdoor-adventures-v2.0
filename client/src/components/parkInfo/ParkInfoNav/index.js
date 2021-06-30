87282dd4f0101c4c17ff85def774f59e04dd0dd2
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-06-29 23:34:52 -0500

    added link to get back to parks page

--- client/src/components/parkInfo/ParkInfoNav/index.js
+++ client/src/components/parkInfo/ParkInfoNav/index.js
@@ -3,35 +3,33 @@ import './ParkInfoNav.css';
 
 const ParkInfoNav = () => {
   return (
-    <div className="park-left-nav-wrapper">
-      <div className="park-left-nav">
-        <div>
-          <Link to="/parks" className="back-link">
-            <i class="fas fa-2x fa-arrow-left"></i>
-          </Link>
-        </div>
-
-        <ul className="no-space-list">
-          <li>
-            <a href="#description">Description</a>
-          </li>
-          <li>
-            <a href="#entrance-fees">Entrance Fees</a>
-          </li>
-          <li>
-            <a href="#hours">Hours</a>
-          </li>
-          <li>
-            <a href="#activites">Actvities</a>
-          </li>
-          <li>
-            <a href="#contact">Contact Info</a>
-          </li>
-          <li>
-            <a href="#photos">Photos</a>
-          </li>
-        </ul>
+    <div className="park-left-nav">
+      <div className="park-left-nav-top">
+        <Link className="back-link" to="/parks">
+          <i class="fas fa-2x fa-arrow-left"></i> Back to parks
+        </Link>
       </div>
+
+      <ul className="no-space-list">
+        <li>
+          <a href="#description">Description</a>
+        </li>
+        <li>
+          <a href="#entrance-fees">Entrance Fees</a>
+        </li>
+        <li>
+          <a href="#hours">Hours</a>
+        </li>
+        <li>
+          <a href="#activites">Actvities</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Info</a>
+        </li>
+        <li>
+          <a href="#photos">Photos</a>
+        </li>
+      </ul>
     </div>
   );
 };
