14fabeac7f10b1df475cfbc112b109cbf922caf0
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-07-05 23:03:58 -0500

    formatted parks page

--- client/src/components/tables/PaginatedTable.js
+++ client/src/components/tables/PaginatedTable.js
@@ -24,7 +24,7 @@ const StyledTableCell = withStyles((theme) => ({
   },
   body: {
     fontSize: '1rem',
-    padding: '11px',
+    padding: '10px',
   },
 }))(TableCell);
 
@@ -157,46 +157,52 @@ const PaginatedTable = ({ history }) => {
 
   return (
     <div className="container-fluid parks-table-container">
-      <div className="row">
-        <div className="col-md-4 col-12 mb-4">
-          <label htmlFor="parkname-input" className="visually-hidden">
-            Park Name
-          </label>
-          <input
-            value={parkName}
-            onChange={onInputChange}
-            type="text"
-            className="form-control parkname-input"
-            id="parkname-input"
-            aria-describedby="parkname"
-            placeholder="Search by park name"
-          />
-        </div>
-        <div className="col-md-4 col-12 mb-4">
-          <label htmlFor="states-select" className="visually-hidden">
-            States Filter
-          </label>
-          <Select
-            name="states-select"
-            placeholder="Filter by State(s)"
-            options={stateList}
-            isMulti
-            onChange={onStateSelectChange}
-          />
-        </div>
-        <div className="col-md-4 col-12 mb-4">
-          <label htmlFor="designation-select" className="visually-hidden">
-            Designation Filters
-          </label>
-          <Select
-            name="designation-select"
-            placeholder="Filter by Deisgnation(s)"
-            options={designationList}
-            isMulti
-            onChange={onDesignationSelectChange}
-          />
+      <div className="container-fluid search-container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="text-center">Search National Parks</h1>
+          </div>
+          <div className="col-md-4 col-12 mb-4">
+            <label htmlFor="parkname-input" className="visually-hidden">
+              Park Name
+            </label>
+            <input
+              value={parkName}
+              onChange={onInputChange}
+              type="text"
+              className="form-control parkname-input"
+              id="parkname-input"
+              aria-describedby="parkname"
+              placeholder="Search by park name"
+            />
+          </div>
+          <div className="col-md-4 col-12 mb-4">
+            <label htmlFor="states-select" className="visually-hidden">
+              States Filter
+            </label>
+            <Select
+              name="states-select"
+              placeholder="Filter by State(s)"
+              options={stateList}
+              isMulti
+              onChange={onStateSelectChange}
+            />
+          </div>
+          <div className="col-md-4 col-12 mb-4">
+            <label htmlFor="designation-select" className="visually-hidden">
+              Designation Filters
+            </label>
+            <Select
+              name="designation-select"
+              placeholder="Filter by Deisgnation(s)"
+              options={designationList}
+              isMulti
+              onChange={onDesignationSelectChange}
+            />
+          </div>
         </div>
       </div>
+
       <TableContainer component={Paper} className={classes.tableContainer}>
         <Table className={classes.table} aria-label="customized table">
           <TableHead>
--- client/src/pages/ParksPage.js
+++ client/src/pages/ParksPage.js
@@ -3,10 +3,6 @@ import PaginatedTable from '../components/tables/PaginatedTable';
 const ParksPage = ({ history }) => {
   return (
     <div className="parks-page-background">
-      <div className="container">
-        <h1>Parks Page</h1>
-      </div>
-
       <PaginatedTable history={history} />
     </div>
   );
