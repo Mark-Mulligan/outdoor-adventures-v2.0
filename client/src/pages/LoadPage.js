71ea29ca3f22df8f821dd05f16925861fd2831c4
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-07-22 22:15:27 -0500

    added comments to loading page

--- client/src/pages/LoadPage.js
+++ client/src/pages/LoadPage.js
@@ -1,5 +1,10 @@
 const LoadPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <h2>You can use the park table to search all the national parks in the united states</h2>
+      <p>The app is loading. Once the loading is complete, it should run as normal with no more wait times.</p>
+    </div>
+  );
 };
 
 export default LoadPage;
